c018b6f60448ab5f9762f6847769bf685191d0e0
rizkianas <112039501+rizkianas@users.noreply.github.com>
2022-12-29 11:32:09 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let n = prompt("masukkan nilai: ");
 for(var i = 1; i <= n; i++){
     if(i % 2 === 0){
-        document.write("odd");
+        document.write(" odd ");
     }else{
-        document.write(`${i}`)
+        document.write(` ${i} `)
     }
 }
